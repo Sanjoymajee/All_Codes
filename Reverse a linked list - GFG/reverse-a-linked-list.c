e5424b6fb9108b87fbccb31b509ffe54af5353b9
Sanjoymajee <54500644+Sanjoymajee@users.noreply.github.com>
2022-03-03 16:11:39 +0530

    Added solution - LeetHub

---
+++ Reverse a linked list - GFG/reverse-a-linked-list.c
@@ -0,0 +1,99 @@
+// { Driver Code Starts
+//Initial Template for C
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+}*start;
+
+void insert();
+void display(struct Node *head);
+
+ // } Driver Code Ends
+//User function Template for C
+struct Node* temp;
+
+struct Node* reverse(struct Node* head)
+{
+    if(!head->next){
+        temp=head;
+        return temp;
+    }
+    struct Node* rest=reverse(head->next);
+    rest->next=head;
+    return head;
+    
+}
+
+struct Node* reverseList(struct Node *head)
+    {
+        if (head == NULL || head->next == NULL)
+            return head;
+        struct Node* rest = reverse(head);
+        rest->next=NULL;
+        return temp;
+        
+    }
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+      start=NULL;
+      insert();
+      start = reverseList(start);
+      display(start);
+      printf("\n");
+    }
+    return 0;
+
+}
+
+
+ void insert()
+ {
+     int n,value,i;
+     scanf("%d",&n);
+     struct Node *temp;
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&value);
+         if(i==0)
+         {
+              start=(struct Node *) malloc( sizeof(struct Node) );
+              start->data=value;
+              start->next=NULL;
+              temp=start;
+              continue;
+         }
+         else
+         {
+             temp->next= (struct Node *) malloc( sizeof(struct Node) );
+             temp=temp->next;
+             temp->data=value;
+             temp->next=NULL;
+         }
+     }
+ }
+ 
+ void display(struct Node *head)
+{
+   while(head!=NULL)
+  {
+   printf("%d ",head->data);
+    head=head->next;
+  }
+}
+
+
+
+
+  // } Driver Code Ends
