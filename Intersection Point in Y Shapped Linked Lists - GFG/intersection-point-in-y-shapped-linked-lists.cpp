a2c39416c97135960c0360439d83d0ec5583474d
Sanjoymajee <54500644+Sanjoymajee@users.noreply.github.com>
2022-03-18 01:32:26 +0530

    Added solution - LeetHub

---
+++ Intersection Point in Y Shapped Linked Lists - GFG/intersection-point-in-y-shapped-linked-lists.cpp
@@ -0,0 +1,108 @@
+// { Driver Code Starts
+#include<iostream>
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+int intersectPoint(struct Node* head1, struct Node* head2);
+
+Node* inputList(int size)
+{
+    if(size==0) return NULL;
+    
+    int val;
+    cin>> val;
+    
+    Node *head = new Node(val);
+    Node *tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,n1,n2,n3;
+
+    cin>>T;
+    while(T--)
+    {
+        cin>>n1>>n2>>n3;
+        
+        Node* head1 = inputList(n1);
+        Node* head2 = inputList(n2);
+        Node* common = inputList(n3);
+        
+        Node* temp = head1;
+        while(temp!=NULL && temp->next != NULL)
+            temp = temp->next;
+        if(temp!=NULL) temp->next = common;
+        
+        temp = head2;
+        while(temp!=NULL && temp->next != NULL)
+            temp = temp->next;
+        if(temp!=NULL) temp->next = common;
+        
+        cout << intersectPoint(head1, head2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+//Function to find intersection point in Y shaped Linked Lists.
+int intersectPoint(Node* headA, Node* headB){
+    Node* t1=headA;
+    Node* t2=headB;
+    int sz1=0,sz2=0;
+    while(t1){
+        sz1++;
+        t1=t1->next;
+    }
+    while(t2){
+        sz2++;
+        t2=t2->next;
+    }
+    if(sz1<sz2)return intersectPoint(headB,headA);
+    int diff=sz1-sz2;
+    t1=headA;
+    t2=headB;
+    while(diff--)t1=t1->next;
+    while(t1 && t2){
+        if(t1==t2)return t1->data;
+        t1=t1->next;
+        t2=t2->next;
+    }
+    return -1;
+}
+
