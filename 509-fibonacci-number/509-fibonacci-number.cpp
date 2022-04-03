8fb117a3e9c71bcf313bad9721b9d48397e33bbc
Sanjoymajee <54500644+Sanjoymajee@users.noreply.github.com>
2022-04-03 12:52:17 +0530

    Time: 5 ms (43.43%), Space: 6.2 MB (10.07%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,57 +1,12 @@
 class Solution {
 public:
-    vector<vector<int>> Mutliply(vector<vector<int> >& mA, vector<vector<int>>&mB) {
-        int n=mA.size();
-        int m=mA[0].size();
-        int r=mB[0].size();
-        vector<vector<int> >Nm(n,vector<int>(m));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < r; j++){
-                Nm[i][j] = 0;
-                for (int k = 0; k < m; k++) {
-                    Nm[i][j] += (mA[i][k] * mB[k][j]);
-                }
-            }
-        }
-        return Nm;
-    }
- 
-    vector<vector<int>> power(vector<vector<int>> M,int y,int n){
-        vector<vector<int>> ans(n,vector<int>(n,0));
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                if(i==j)ans[i][j]=1;
-            }
-        }
-        while(y){
-            if(y%2){
-                ans=Mutliply(ans,M);
-                y--;
-            }
-            else{
-                M=Mutliply(M,M);
-                y/=2;
-            }
-        }
-        return ans;
-    }
-    
     int fib(int n) {
         if(n==0)return 0;
-        if(n==1)return 1;
-        vector<vector<int>> M={{0,1},{1,1}};
-        vector<vector<int>> Pm=power(M,n-1,2);
-        // for(auto a:Pm){
-        //     for(auto b:a)cout<<b<<" ";
-        //     cout<<endl;
-        // }
-        vector<vector<int>> P={{0,1}};
-        Pm=Mutliply(P,Pm);
-        // for(auto a:Pm){
-        //     for(auto b:a)cout<<b<<" ";
-        //     cout<<endl;
-        // }
-        return Pm[0][1];
-        
+        vector<int> dp(n+1,0);
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
