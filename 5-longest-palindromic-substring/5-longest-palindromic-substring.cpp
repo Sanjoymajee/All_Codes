4d3ea6a3f4ea3ae2751bae3dfb4ac6cf5594f6f3
Sanjoymajee <54500644+Sanjoymajee@users.noreply.github.com>
2022-04-21 20:17:06 +0530

    Time: 4 ms (99.04%), Space: 7.1 MB (81.86%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,36 +2,20 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n=s.size();
-        vector<vector<int>>dp(n,vector<int>(n));
-        int i=0,j=0;
-        while(i<n && j<n){
-            int k=i,l=j;
-            while(i<n && j<n){
-                if(i==j)dp[i][j]=1;
-                else{
-                    if(s[i]==s[j]){
-                        if(j-i==1)dp[i][j]=1;
-                        else dp[i][j]=dp[i+1][j-1];
-                    }
-                }
-                i++;
-                j++;
-            }
-            i=k,j=l;
-            j++;
-        }
-        int ans=0;
         pair<int,int>cord;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(dp[i][j]){
-                    if(ans<j-i+1){
-                        ans=j-i+1;
-                        cord={i,j};
-                    }
-                }
+        int len=0;
+        for(int i=0;i<n;){
+            int l=i;
+            int r=i;
+            while(r<n-1 && s[r]==s[r+1])r++;
+            i=r+1;
+            while(l>0 && r<n && s[l-1]==s[r+1])l--,r++;
+            int temp=r-l+1;
+            if(temp>=len){
+                len=temp;
+                cord={l,r};
             }
         }
-        return s.substr(cord.first,cord.second-cord.first+1);
+        return s.substr(cord.first,len);
     }
 };
