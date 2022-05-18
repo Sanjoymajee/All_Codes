3e3c76b9fa63b1773e96f00537e06b5416e8caf0
Sanjoymajee <54500644+Sanjoymajee@users.noreply.github.com>
2022-05-18 14:55:39 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -12,33 +12,27 @@ class Solution {
     const int INF=1000000000;
     
 	void shortest_distance(vector<vector<int>>&matrix){
-	    int V=matrix.size();
-	    int dist[V][V];
-	    for(int i=0;i<V;i++){
-	        for(int j=0;j<V;j++){
+	    int n=matrix.size();
+	    int dist[n][n];
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<n;j++){
 	            if(matrix[i][j]!=-1)
 	                dist[i][j]=matrix[i][j];
                 else dist[i][j]=INF;
 	        }
 	    }
-	         // Pick all vertices as source one by one
-        for (int k = 0; k < V; k++) {
-        for (int i = 0; i < V; i++) {
-            // Pick all vertices as destination for the
-            // above picked source
-            for (int j = 0; j < V; j++) {
-                // If vertex k is on the shortest path from
-                // i to j, then update the value of
-                // dist[i][j]
-                if (dist[i][j] > (dist[i][k] + dist[k][j])
-                    && (dist[k][j] != INF
-                        && dist[i][k] != INF))
+	    for(int k=0;k<n;k++){
+	        for(int i=0;i<n;i++){
+	            for(int j=0;j<n;j++){
+	                if (dist[i][j] > (dist[i][k] + dist[k][j])
+                        && (dist[k][j] != INF
+                            && dist[i][k] != INF))
                     dist[i][j] = dist[i][k] + dist[k][j];
-            }
-        }
-    }
-	    for(int i=0;i<V;i++){
-	        for(int j=0;j<V;j++){
+	            }
+	        }
+	    }
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<n;j++){
 	            if(dist[i][j]!=INF)
 	                matrix[i][j]=dist[i][j];
                 else matrix[i][j]=-1;
